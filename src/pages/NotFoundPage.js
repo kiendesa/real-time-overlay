b3b0bf11fcb08e91f14eb5121a06476f2f5bb496
kiendesa <nguyencongkien@g.densan-ginza.co.jp>
2024-10-03 14:52:12 +0900

    edit error

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+    return (
+        <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
+            <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
+            <h2 className="text-2xl font-semibold text-gray-800 mb-2">Page Not Found</h2>
+            <p className="text-gray-600 mb-6">Sorry, the page you're looking for doesn't exist.</p>
+            <Link to="/" className="text-blue-500 hover:underline text-lg">
+                Go back to Homepage
+            </Link>
+        </div>
+    );
+};
+
+export default NotFoundPage;
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
+import NotFoundPage from '../pages/NotFoundPage';
+
 
 const AppRoutes = () => {
     return (
         <Router basename="/meet-image">
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </Router>
     );
