414cba0945c6c60891bc60454f9661f421e16448
kiendesa <nguyencongkien@g.densan-ginza.co.jp>
2024-10-04 10:55:22 +0900

    error handling

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
             <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
             <h2 className="text-2xl font-semibold text-gray-800 mb-2">Page Not Found</h2>
             <p className="text-gray-600 mb-6">Sorry, the page you're looking for doesn't exist.</p>
-            <Link to="/" className="text-blue-500 hover:underline text-lg">
+            <Link to="/meet-image" className="text-blue-500 hover:underline text-lg">
                 Go back to Homepage
             </Link>
         </div>
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import NotFoundPage from '../pages/NotFoundPage';
 
+const router = createBrowserRouter([
+    {
+        path: "/meet-image",
+        element: <HomePage />
+    },
+    {
+        path: "*",
+        element: <NotFoundPage />,
+    },
+])
 
 const AppRoutes = () => {
     return (
-        <Router basename="/meet-image">
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-        </Router>
+        <div className='app'>
+            <RouterProvider router={router} />
+        </div>
     );
 };
 
